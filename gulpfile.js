774dba082348eb41b360d017a7dd4450295fd084
Weyland404 <weyland404@p0mp.com>
2014-04-22 22:39:12 +0200

    Moved some stuff and updated files

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,65 @@
 var gulp = require('gulp'),
-	livereload = require('gulp-livereload'),
-	path = require('path'),
-	jade = require('gulp-jade'),
-	compass = require('gulp-compass'),
-	dest = '';
+    livereload = require('gulp-livereload'),
+    path = require('path'),
+    jade = require('gulp-jade'),
+    compass = require('gulp-compass'),
+    dest = '';
 
-gulp.task('default', ['once', 'server', 'watch']);
+gulp.task('default', ['once']);
 
-gulp.task('server', function(next) {
-	require('./server');
+gulp.task('server', function (next) {
+    //require('./server');
 });
 
-gulp.task('once', ['jade', 'compass', 'js']);
+gulp.task('once', [
+    'jade',
+    'sass',
+    'js',
+    'img'
+]);
 
-gulp.task('jade', function() {
-	  var YOUR_LOCALS = {};
-  gulp.src('./dist/*.jade')
-    .pipe(jade({
-      locals: YOUR_LOCALS
-    }))
-    .on('error', function(err) {
-    	console.error('Jade could not be compiled\n'+err.message);
-        // Would like to catch the error here
-    })
-    .pipe(gulp.dest('./public/'))
-});
-gulp.task('bower', function() {
-    gulp.src('./bower_components/*/*.js', {base: './bower_components/'})
-  .pipe(gulp.dest('./public/vendor/'));
-});
-gulp.task('js', function() {
-    gulp.src('./dist/*.js', {base: './dist/'})
-  .pipe(gulp.dest('./public/'));
-});
-gulp.task('compass', function() {
-    gulp.src('./dist/*.sass')
-    .pipe(compass({
-            config_file: './config.rb',
-            css: 'public',
-            sass: 'dist'
+gulp.task('jade', function () {
+    var YOUR_LOCALS = {};
+    gulp.src(['./client/jade/*.jade', './client/jade/**/*.jade'], {base: './client/jade/'})
+        .pipe(jade({
+            locals: YOUR_LOCALS
         }))
-    .on('error', function(err) {
-    	console.error('Compass could not be compiled\n'+err.message);
-    })
-    .pipe(gulp.dest('public'));
+        .on('error', function (err) {
+            console.error('Jade could not be compiled\n' + err.message);
+        })
+        .pipe(gulp.dest('./build/'))
+});
+gulp.task('bower', function () {
+    gulp.src('./bower_components/*/*.js', {base: './bower_components/'})
+        .pipe(gulp.dest('./build/vendor/'));
+});
+gulp.task('img', function () {
+    gulp.src('./client/img/**', {base: './client/'})
+        .pipe(gulp.dest('./build/'));
+});
+gulp.task('js', function () {
+    gulp.src('./client/js/*.js', {base: './client/'})
+        .pipe(gulp.dest('./build/'));
+});
+gulp.task('sass', function () {
+    gulp.src('./client/sass/*.sass')
+        .pipe(compass({
+            css: 'build/css',
+            sass: 'client/sass'
+        }))
+        .on('error', function (err) {
+            console.error('Sass could not be compiled\n' + err.message);
+        })
+        .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('watch', function() {
-	var server = livereload();
-	var publicSourceFolder = path.join(__dirname, 'dist');
-	gulp.watch('./public/**/*').on('change', function(file) {
-		console.log('Change', file);
-		server.changed(file.path);
-	});
-	gulp.watch('./dist/**/*.sass', ['compass']);
-    gulp.watch('./dist/**/*.jade', ['jade']);
-    gulp.watch('./dist/**/*.js', ['js']);
+gulp.task('watch', function () {
+    var server = livereload();
+    gulp.watch('./build/**/*').on('change', function (file) {
+        console.log('Change', file);
+        server.changed(file.path);
+    });
+    gulp.watch('./client/**/*.sass', ['sass']);
+    gulp.watch('./client/**/*.jade', ['jade']);
+    gulp.watch('./client/**/*.js', ['js']);
 });
