97afca8857dfaf2731a1452fdaa0a662e619a04c
wvbe <wybe@x-54.com>
2014-04-28 02:44:36 +0200

    better now.

--- client/js/directive/ds-button.js
+++ client/js/directive/ds-button.js
@@ -11,41 +11,56 @@ angular.module('xf.dualshock.dsbutton', ['xf.dualshock'])
 					lastTapped = 0,
 					state = {};
 
-				function press(keyName) { // registers a press, not the same as (eg. doubletap "activate")
-					state[keyName] = true;
-
-					if(keyNames.length>1) // if key combination
-						return combopress(keyName);
-
-					activate(keyName);
-				}
 				function isPressed() {
 					for(var i=0;i<keyNames.length;i++) // for each key
 						if(!state[keyNames[i]]) // when not true
 							return false;
 					return true;
 				}
-				function combopress(keyName) {
-					if(isPressed())
-						return activate('combo-pressed');
-					return deactivate('combo-pressed');
 
+				function press(keyName) { // registers a press, not the same as (eg. doubletap "activate")
+					state[keyName] = true;
+					var prssd = true;
+					if(keyNames.length>1) // if key combination
+						prssd = combopress(keyName);
+					if (doubleTapDelay && prssd)
+						prssd = doubletap(keyName);
+					if(prssd)
+						activate(keyName);
 				}
+				function combopress(keyName) {
+					if(isPressed()) {
+						$element.addClass('combo-pressed');
+						return true;
+					}
+					$element.removeClass('combo-pressed');
+					return false;
+				}
+
 				function doubletap(keyName) {
-					var nowTapped = new Date().getTime();
-					if (lastTapped && (nowTapped - lastTapped) <= doubleTapDelay)
-						activate('double-tapped');
-					else
-						deactivate('double-tapped');
+					var nowTapped = new Date().getTime(), double = false;
+					if (lastTapped && (nowTapped - lastTapped) <= doubleTapDelay) {
+						$element.addClass('double-tapped');
+						double = true;
+					} else {
+						$element.removeClass('double-tapped');
+
+					}
 					lastTapped = nowTapped;
+					return double;
 				}
+
+
 				function release(keyName) {
 					state[keyName] = false;
-					$scope.pressed = false;
-					deactivate();
+					var modifiers = [];
+					if(keyNames.length) modifiers.push('combo-pressed');
+					if(doubleTapDelay) modifiers.push('double-tapped');
+					return deactivate();
+					// return deactivate(modifiers.join(' ')); // uncomment to wipe all modifier classnames on release
 				}
-				function activate(className) {
 
+				function activate(className) {
 					if(className)
 						$element.addClass(className);
 
@@ -55,25 +70,21 @@ angular.module('xf.dualshock.dsbutton', ['xf.dualshock'])
 					$scope.pressed = true;
 					$element.addClass('pressed').removeClass('released');
 				}
+
 				function deactivate(className) {
 					$scope.pressed = false;
 					$element.addClass('released').removeClass('pressed');
 					if(className)
 						$element.removeClass(className);
-
 				}
 
 				release();
 				keyNames.map(function(keyName) {
 					$rootScope.$on('dualshock:button:' + keyName + ':press', function () {
 						press(keyName);
-						if (doubleTapDelay && isPressed())
-							doubletap(keyName);
 					});
 					$rootScope.$on('dualshock:button:' + keyName + ':release', function () {
 						release(keyName);
-						if(keyNames.length>1) // if key combination
-							return combopress(keyName);
 					});
 				});
 			},
